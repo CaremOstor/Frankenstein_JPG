61c1f0484658286a343e5648e3e6503b73d17570
podzorovegor <95075197+podzorovegor@users.noreply.github.com>
2022-05-04 16:20:43 +0300

    Update presetting.sh

--- presetting.sh
+++ presetting.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
-sudo apt update && apt upgrade
-sudo apt install ansible 
-sudo useradd -m ansible-admin
+apt update && apt upgrade
+if apt -qq list ansible 2>/dev/null | grep -q installed ;
+then
+ echo "Anisble is installed"
+else
+ apt install ansible
+fi
+useradd -m ansible-admin 
 echo "Enter username of remote host and his ip address"
 read varname 
 read varname1
 echo "Username: $varname and his ip address: $varname1"
+File=/root/.ssh/id_rsa.pub
+if [ -f "$File "]
+then
+ echo "$File exists."
+else
 ssh-keygen
+fi
 ssh-copy-id -i ~/.ssh/id_rsa.pub $varname@$varname1 -p 6576
 
