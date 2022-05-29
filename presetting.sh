c44ba45b9dc6c2e405b7efbb3bf8e527ac3d847f
CaremOstor <102664039+CaremOstor@users.noreply.github.com>
2022-05-29 15:24:37 +0200

    Update presetting.sh

--- presetting.sh
+++ presetting.sh
@@ -1,16 +1,13 @@
+
 #!/bin/bash
-apt update && apt upgrade
 if apt -qq list ansible 2>/dev/null | grep -q installed ;
 then
  echo "Anisble is installed"
 else
  apt install ansible
 fi
-useradd -m ansible-admin 
-echo "Enter username of remote host and his ip address"
-read varname 
-read varname1
-echo "Username: $varname and his ip address: $varname1"
+useradd -m -s /bin/bash ansible-admin
+passwd ansible-admin
 File=/root/.ssh/id_rsa.pub
 if [ -f "$File "]
 then
@@ -18,5 +15,29 @@ then
 else
 ssh-keygen
 fi
-ssh-copy-id -i ~/.ssh/id_rsa.pub $varname@$varname1 -p 6576
-
+echo "Choose variant for sending ssh public key:"
+echo "1 - Enter username and ip address of host"
+echo "2 - Enter subnet and subnet mask of your network"
+read variant;
+case $variant in
+  1) read -p "Username: " username && read -p "Ip address: " ip_addr && ssh-copy-id -i ~/.ssh/id_rsa.pub $username@$ip_addr -p 6576;;
+  2) ping_check()
+{
+  ping -c 1 $1 > /dev/null
+  [ $? -eq 0 ] && echo $i 
+}
+echo "Enter your subnet(only first 3 octets):"
+read subnet
+for i in $subnet.{1..254} 
+do
+ping_check $i >> hosts & disown 
+done
+cat hosts
+hosts=$(cat hosts)
+for prm in $hosts
+do
+        read -p "Enter username for $prm:" username
+        ssh-copy-id -i ~/.ssh/id_rsa.pub $username@$prm -p 6576
+done;;
+   *) echo "Choose the correct option";;
+esac
